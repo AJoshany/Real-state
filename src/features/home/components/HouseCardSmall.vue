ed0ab403fb3a12df47818249d75d90db188d1011
Amir Shariati <amirshrt1382@gmail.com>
2025-09-19 11:54:55 +0330

    feat: create house card small component

---
+++ src/features/home/components/HouseCardSmall.vue
@@ -0,0 +1,103 @@
+<script setup>
+defineProps({
+  house: {
+    type: Object,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <figure class="house-card">
+    <div class="image__container">
+      <img class="house__image" :src="house.gallery[0]" alt="house image" />
+    </div>
+    <div class="card__detail">
+      <div class="card__content">
+        <h3 class="house__name">{{ house.name }}</h3>
+        <span class="house__price">${{ house.price }}/month</span>
+        <p class="house__location">
+          <img src="/icons/location-icon.svg" alt="location icon" />
+          <span>{{ house.address }}</span>
+        </p>
+      </div>
+      <span class="card__bookmark">
+        <img src="/icons/bookmark-icon.svg" alt="bookmark icon" />
+      </span>
+    </div>
+  </figure>
+</template>
+
+<style lang="scss" scoped>
+.house-card {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1.5rem;
+
+  width: clamp(15rem, 25rem, 35rem);
+  padding: 1.4rem;
+  border-radius: 1rem;
+  box-shadow: 0 0 20px 10px rgb(0, 0, 0, 0.1);
+  transition: transform 0.3s ease;
+  cursor: pointer;
+
+  &:hover {
+    transform: translateY(-0.2rem);
+  }
+}
+
+.image__container {
+  overflow: hidden;
+
+  width: 100%;
+  height: 15rem;
+  background-color: var(--color-gray-100);
+  border-radius: 10px;
+}
+
+.house__image {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  border-radius: 10px;
+}
+
+.card__detail {
+  display: flex;
+  justify-content: space-between;
+  align-items: end;
+
+  width: 100%;
+}
+
+.card__content {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+
+  h3 {
+    font-size: 1.5rem;
+    color: var(--color-text-primary);
+    font-weight: 600;
+  }
+}
+
+.house__price {
+  font-size: 1.1rem;
+  color: var(--color-primary);
+}
+
+.house__location {
+  display: flex;
+  gap: 0.2rem;
+  font-size: 1.1rem;
+  color: var(--color-gray-100);
+}
+
+.card__bookmark {
+  padding: 0.9rem 1.1rem;
+  background-color: var(--color-primary-thin);
+  border-radius: 12px;
+}
+</style>
