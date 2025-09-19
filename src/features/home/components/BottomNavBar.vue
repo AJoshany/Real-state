203a837dcdb5adf922a073c2d4ce4eb79af9cb05
Ali Joshany <aa6joshany@gmail.com>
2025-09-19 10:55:29 +0330

    feat:house detailed card and navbar just created

---
+++ src/features/home/components/BottomNavBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <nav class="nav-main">navbar</nav>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.nav-main {
+  width: 100%;
+  height: 64px;
+  position: fixed;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  z-index: 10;
+}
+</style>
---
+++ src/features/home/components/HouseCardDetailed.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Detailed</div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
