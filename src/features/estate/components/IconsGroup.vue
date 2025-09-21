59d94b90dec8054b1cb78c1b8d5cd64bae71ab05
Amir Shariati <amirshrt1382@gmail.com>
2025-09-21 23:03:06 +0330

    feat: create description component

---
+++ src/features/estate/components/IconsGroup.vue
@@ -0,0 +1,41 @@
+<template>
+  <section class="icons-section">
+    <h2 v-if="sectionName" class="icons-header">
+      {{ sectionName.charAt(0).toUpperCase() + sectionName.slice(1) }}
+    </h2>
+    <ul class="icons">
+      <slot></slot>
+    </ul>
+  </section>
+</template>
+
+<script setup>
+defineProps({
+  house: {
+    type: Object,
+    required: true,
+  },
+  sectionName: {
+    type: String,
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.icons-section {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  padding: 2rem;
+}
+
+.icons {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 10px;
+}
+
+.icons-header {
+  font-size: 1.5rem;
+}
+</style>
